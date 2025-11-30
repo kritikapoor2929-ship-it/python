697cf0c3dd72d6ca96cd17ff0f5c4cecd9047a16
Kriti Kapoor <kritikapoor2929@gmail.com>
2025-12-01 01:06:41 +0530

    Add files via upload
    Assignment-3(b)

---
+++ numpy_test.py
@@ -0,0 +1,28 @@
+# -----------------------------
+# Creating a 1D NumPy Array
+# -----------------------------
+array_1d = np.array([10, 20, 30, 40, 50])
+
+# -----------------------------
+# Creating a 2D NumPy Array
+# -----------------------------
+array_2d = np.array([[1, 2, 3],
+                     [4, 5, 6]])
+
+# -----------------------------
+# Displaying properties of 1D array
+# -----------------------------
+print("----- 1D Array -----")
+print("Array:\n", array_1d)
+print("Shape:", array_1d.shape)
+print("Dimensions:", array_1d.ndim)
+print("Data Type:", array_1d.dtype)
+
+# -----------------------------
+# Displaying properties of 2D array
+# -----------------------------
+print("\n----- 2D Array -----")
+print("Array:\n", array_2d)
+print("Shape:", array_2d.shape)
+print("Dimensions:", array_2d.ndim)
+print("Data Type:", array_2d.dtype)
